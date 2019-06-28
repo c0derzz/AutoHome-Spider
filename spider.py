b456aeb5fbbc68b9dbddaaa8492cd0436ac99d9c
ruichuan.li <ruichuan.li@jimu.com>
2019-06-28 15:22:25 +0800

    初始化工程

--- spider.py
+++ spider.py
@@ -1,16 +1,22 @@
 # encoding: utf-8
 import requests
-import setting
+import re
 from bs4 import BeautifulSoup
 
-resp = requests.get("http://www.baidu.com")
-resp.encoding = resp.apparent_encoding
-print(resp.apparent_encoding)
-print(resp.encoding)
-html = resp.text
-print("获取的结果为:",resp.content)
-
+def getHtml(url):
+    matcherObj = re.match(r'[http|https]+://[^\s]*[.com|.cn]',url)
+    #正则校验是否是合法的url 非法url不处理
+    if matcherObj:
+        resp_for_url = requests.get(url)
+        if resp_for_url.status_code == 200:
+            resp_for_url.encoding = resp_for_url.apparent_encoding
+            return resp_for_url.text
+        return ""
+    else:
+        print("unallowable character:",url)
+html = getHtml("http://www.baidu.com")
+print(html)
 soup = BeautifulSoup(html,"html.parser")
-
-print(soup.prettify())
-print(soup.find_all('a'))
+a_list = soup.find_all('a')
+for a in a_list:
+    print(getHtml(a.get('href')))
